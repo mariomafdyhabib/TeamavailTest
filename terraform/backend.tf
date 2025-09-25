4bdfa98ebabcee9c31c3f2b068e0619292f716f9
mariomafdyhabib <mariomafdy333@gmail.com>
2025-09-25 04:00:41 +0300

    edit pipe

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "ymariomafdy" 
+    organization = "mariomafdy" 
 
     workspaces {
       name = "TeamavailTest"   
